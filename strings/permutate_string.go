78bd931f6238a479fb3e6f8aa7f6997e58d0cd8e
nagarjuna gowtham karuturi <gowthamkaruturi@gmail.com>
2021-01-21 14:44:10 -0500

    Create permutate_string.go

---
+++ strings/permutate_string.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func permute(strs []string) {
+	m := make(map[string]int)
+
+	for _, ch := range strs {
+		val, ok := m[ch]
+		fmt.Println("val is ", val, ok)
+		if ok {
+			m[ch] = m[ch] + 1
+		} else {
+			fmt.Println("i am here")
+			m[ch] = 1
+
+		}
+	}
+
+	charArray := make([]string, len(m))
+	lengthArray := make([]int, len(m))
+	index := 0
+	for k, v := range m {
+		fmt.Println("key and values are ", k, v)
+		charArray[index] = k
+		lengthArray[index] = v
+		index++
+	}
+	resultArray := make([]string, len(strs))
+	permuteUtil(charArray, lengthArray, resultArray, 0)
+
+}
+
+func permuteUtil(str []string, count []int, result []string, level int) {
+	if level == len(result) {
+		fmt.Println(result)
+	}
+
+	for i := 0; i < len(str); i++ {
+		if count[i] == 0 {
+			continue
+		}
+		result[level] = str[i]
+		count[i]--
+		permuteUtil(str, count, result, level+1)
+		count[i]++
+
+	}
+}
+func main() {
+	newArr := strings.Split("gowthammmmmm", "")
+	fmt.Println(newArr)
+	permute(newArr)
+
+}
