48020f8e20eea32d65e469cdd70478e4db275605
gowthamkaruturi <gowthamkaruturi@gmail.com>
2022-01-12 06:49:45 -0500

    changes to algo

--- arrays/knapsacksolve.go
+++ arrays/knapsacksolve.go
@@ -1,32 +1,38 @@
 package arrays
 
-import "math"
+import (
+	"fmt"
+	"math"
+)
 
-func knapcsacksolve(profits []int, weights []int, capacity int) int {
+func Knapcsacksolve(profits []int, weights []int, capacity int) int {
 	// basic check
 	if len(profits) == 0 || len(weights) == 0 || len(profits) != len(weights) || capacity == 0 {
 		return 0
 	}
 	n := len(profits)
 	dp := make([]int, capacity+1)
-	// single check
+	// single check if there is only one weight
 	for c := 0; c <= capacity; c++ {
-		if weights[0] <= capacity {
+		if weights[0] <= c {
 			dp[c] = profits[0]
 		}
 	}
 
 	for i := 1; i < n; i++ {
-		for c := capacity; c >= 0; c++ {
+		for c := capacity; c >= 0; c-- {
 			profit2 := 0
 			profit1 := 0
+			if weights[i] <= c {
+				profit1 = profits[i] + dp[c-weights[i]]
+			}
 
-			profit1 = profits[i] + dp[c-weights[i]]
 			profit2 = dp[c]
 
 			dp[c] = int(math.Max(float64(profit1), float64(profit2)))
 		}
 	}
+	fmt.Println(dp)
 	return dp[capacity]
 
 }
