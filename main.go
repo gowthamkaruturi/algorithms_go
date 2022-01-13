c97baae255d71a0445baa293d459b8e335bedc53
gowthamkaruturi <gowthamkaruturi@gmail.com>
2022-01-12 20:40:28 -0500

    changes to subset

--- main.go
+++ main.go
@@ -8,4 +8,9 @@ import (
 
 func main() {
 	fmt.Println(arrays.Fiboatn(65))
+	var a [5]int
+	fmt.Println(arrays.Fibo(4, a[0:5]))
+	profits := []int{1, 6, 10, 16}
+	weights := []int{1, 2, 3, 5}
+	fmt.Println(arrays.Knapcsacksolve(profits, weights, 7))
 }
